009412fa732ef057b68781cb743f40a61ec44758
Kristina Jumanova <kristya1310@gmail.com>
2022-11-10 22:04:33 +0700

    feat: middleware adding  user id

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT);
