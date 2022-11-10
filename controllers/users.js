b1a135252881024f78f4f7fe0bffe2a7d4e2a65d
Kristina Jumanova <kristya1310@gmail.com>
2022-11-10 21:41:12 +0700

    feat: controllers for user

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((newUser) => res.status(201).send(newUser))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.getUserById = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(Number(userId))
+    .then((user) => res.status(200).send(user))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
