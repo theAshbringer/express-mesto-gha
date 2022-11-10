bbe214e7345d8bac839f466a5ccc20d69bef9545
Kristina Jumanova <kristya1310@gmail.com>
2022-11-10 20:36:33 +0700

    feat: models for card and user

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
