11fdc4faf4c92733a3c966a964cce460eb70eccb
Kristina Jumanova <kristya1310@gmail.com>
2022-11-15 20:07:24 +0700

    fix: x2

--- routes/cards.js
+++ routes/cards.js
@@ -2,16 +2,15 @@ const router = require('express').Router();
 const {
   createCard, getCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
-const validateCard = require('../utils/middleware/validateCard');
 
 router.get('/', getCards);
 
 router.post('/', createCard);
 
-router.delete('/:cardId', validateCard, deleteCard);
+router.delete('/:cardId', deleteCard);
 
-router.put('/:cardId/likes', validateCard, likeCard);
+router.put('/:cardId/likes', likeCard);
 
-router.delete('/:cardId/likes', validateCard, dislikeCard);
+router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
--- utils/common.js
+++ utils/common.js
@@ -1,15 +1,7 @@
 const validate = require('mongoose-validator');
 const validator = require('validator');
 
-const { DEFAULT_ERROR, INVALID_DATA, NOT_FOUND } = require('./constants');
-
 const throwMessage = (message) => ({ message });
-
-const throwDefaultError = (res, mes) => res.status(DEFAULT_ERROR).send(throwMessage(mes));
-
-const throwValidationError = (res, mes) => res.status(INVALID_DATA).send(throwMessage(mes));
-
-const throwNotFoundError = (res, mes) => res.status(NOT_FOUND).send(throwMessage(mes));
 
 const urlValidator = validate({
   validator: (value) => validator.isURL(
@@ -25,8 +17,5 @@ const urlValidator = validate({
 
 module.exports = {
   throwMessage,
-  throwDefaultError,
-  throwValidationError,
-  throwNotFoundError,
   urlValidator,
 };
