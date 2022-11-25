3ce28c159fa3fa5d8113ee21742a8e4493ffdfce
theAshbringer <kristya1310@gmail.com>
2022-11-26 00:17:13 +0700

    feat: validation and passing errors for cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,97 +3,64 @@ const {
   SUCCESS,
   CREATED,
   CARD_DELETED,
-  INVALID_DATA,
-  DEFAULT_ERROR,
-  VALIDATION_ERROR,
-  MSG_INVALID_CARD_DATA,
-  NOT_FOUND_ERROR,
-  CAST_ERROR, NOT_FOUND,
-  MSG_CARD_NOT_FOUND,
   MSG_MISSING_AUTH_HEADER,
+  MSG_CARD_NOT_FOUND,
 } = require('../utils/constants');
 const { throwMessage } = require('../utils/common');
+const NotFoundError = require('../errors/not-found-err');
+const UnauthorizedError = require('../errors/unauth-err');
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user })
     .then((newCard) => res.status(CREATED).send(newCard))
-    .catch((err) => {
-      if (err.name === VALIDATION_ERROR) {
-        return res.status(INVALID_DATA).send(throwMessage(MSG_INVALID_CARD_DATA));
-      }
-      return res.status(DEFAULT_ERROR).send(throwMessage(err.message));
-    });
+    .catch(next);
 };
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.status(SUCCESS).send(cards))
-    .catch((err) => res.status(DEFAULT_ERROR).send(throwMessage(err.message)));
+    .catch(next);
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   const { cardId } = req.params;
   const { _id } = req.user;
 
-  Card.findById(cardId).orFail({ name: NOT_FOUND_ERROR })
+  Card.findById(cardId).orFail(new NotFoundError(MSG_CARD_NOT_FOUND))
     // eslint-disable-next-line consistent-return
     .then((card) => {
       // eslint-disable-next-line eqeqeq
       if (card.owner != _id) {
-        return Promise.reject(new Error(MSG_MISSING_AUTH_HEADER));
+        return Promise.reject(new UnauthorizedError(MSG_MISSING_AUTH_HEADER));
       }
     })
-    .then(() => Card.deleteOne({ _id: cardId }).orFail({ name: NOT_FOUND_ERROR }))
+    .then(() => Card.deleteOne({ _id: cardId }).orFail(new NotFoundError(MSG_CARD_NOT_FOUND)))
     .then(() => res.status(SUCCESS).send(throwMessage(CARD_DELETED)))
-    .catch((err) => {
-      if (err.name === CAST_ERROR) {
-        return res.status(INVALID_DATA).send(throwMessage(MSG_INVALID_CARD_DATA));
-      }
-      if (err.name === NOT_FOUND_ERROR) {
-        return res.status(NOT_FOUND).send(throwMessage(MSG_CARD_NOT_FOUND));
-      }
-      return res.status(DEFAULT_ERROR).send(throwMessage(err.message));
-    });
+    .catch(next);
 };
 
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   const { cardId } = req.params;
 
   Card.findByIdAndUpdate(
     cardId,
     { $addToSet: { likes: req.user._id } },
     { returnDocument: 'after' },
-  ).orFail({ name: NOT_FOUND_ERROR })
+  ).orFail(new NotFoundError(MSG_CARD_NOT_FOUND))
     .then((card) => res.status(SUCCESS).send({ likes: card.likes }))
-    .catch((err) => {
-      if (err.name === CAST_ERROR) {
-        return res.status(INVALID_DATA).send(throwMessage(MSG_INVALID_CARD_DATA));
-      }
-      if (err.name === NOT_FOUND_ERROR) {
-        return res.status(NOT_FOUND).send(throwMessage(MSG_CARD_NOT_FOUND));
-      }
-      return res.status(DEFAULT_ERROR).send(throwMessage(err.message));
-    });
+    .catch(next);
 };
 
-module.exports.dislikeCard = (req, res) => {
+module.exports.dislikeCard = (req, res, next) => {
   const { cardId } = req.params;
 
   Card.findByIdAndUpdate(
     cardId,
     { $pull: { likes: req.user._id } },
     { returnDocument: 'after' },
-  ).orFail({ name: NOT_FOUND_ERROR })
+  ).orFail(new NotFoundError(MSG_CARD_NOT_FOUND))
     .then((card) => res.status(SUCCESS).send({ likes: card.likes }))
-    .catch((err) => {
-      if (err.name === CAST_ERROR) {
-        return res.status(INVALID_DATA).send(throwMessage(MSG_INVALID_CARD_DATA));
-      }
-      if (err.name === NOT_FOUND_ERROR) {
-        return res.status(NOT_FOUND).send(throwMessage(MSG_CARD_NOT_FOUND));
-      }
-      return res.status(DEFAULT_ERROR).send(throwMessage(err.message));
-    });
+    .catch(next);
 };
--- routes/cards.js
+++ routes/cards.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 const {
   createCard, getCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
+const { cardIdValidator, cardDataValidator } = require('../validators/cards');
 
 router.get('/', getCards);
 
-router.post('/', createCard);
+router.post('/', cardDataValidator, createCard);
 
-router.delete('/:cardId', deleteCard);
+router.delete('/:cardId', cardIdValidator, deleteCard);
 
-router.put('/:cardId/likes', likeCard);
+router.put('/:cardId/likes', cardIdValidator, likeCard);
 
-router.delete('/:cardId/likes', dislikeCard);
+router.delete('/:cardId/likes', cardIdValidator, dislikeCard);
 
 module.exports = router;
