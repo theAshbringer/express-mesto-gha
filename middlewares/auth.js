1f5a6f945de352a22e3d8421c466307596afef72
theAshbringer <kristya1310@gmail.com>
2022-11-26 02:10:41 +0700

    fix: auth by cookie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,8 +7,7 @@ const extractBearerToken = (header) => header.replace('Bearer ', '');
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
-  if (!req.cookies.jwt || !authorization || !authorization.startsWith('Bearer ')) {
+  if (!req.cookies.jwt && (!authorization || !authorization.startsWith('Bearer '))) {
     return res.status(UNAUTHORIZED).send(throwMessage(MSG_MISSING_AUTH_HEADER));
   }
 
