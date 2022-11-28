1981d22c11c59881b2f46eb6a1857f285413214e
theAshbringer <kristya1310@gmail.com>
2022-11-28 22:40:21 +0700

    ref: move error handler to a middleware

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const {
-  MSG_ROUTE_NOT_FOUND, DEFAULT_ERROR, MSG_DEFAULT,
+  MSG_ROUTE_NOT_FOUND,
 } = require('./utils/constants');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const signupValidator = require('./validators/signup-validator');
 const signinValidator = require('./validators/signin-validator');
 const NotFoundError = require('./errors/not-found-err');
+const errorHandler = require('./middlewares/errorHandler');
 
 require('dotenv').config();
 
@@ -27,7 +27,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(cookieParser());
 
 app.use('/signin', signinValidator, login);
-
 app.use('/signup', signupValidator, createUser);
 
 app.use(auth);
@@ -35,16 +34,10 @@ app.use(auth);
 app.use('/cards', require('./routes/cards'));
 app.use('/users', require('./routes/users'));
 
-app.use(errors());
+app.use(errors()); // handling Joi errors
 
 app.use((req, res, next) => next(new NotFoundError(MSG_ROUTE_NOT_FOUND)));
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || DEFAULT_ERROR;
-
-  const message = statusCode === DEFAULT_ERROR ? MSG_DEFAULT : err.message;
-  res.status(statusCode).send({ message });
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT);
