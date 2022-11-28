90d878b5b9fbaab458a050968396648e413b271c
theAshbringer <kristya1310@gmail.com>
2022-11-28 21:32:48 +0700

    ref: rm parsing for authorization header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,16 +2,13 @@ const jwt = require('jsonwebtoken');
 const { throwMessage } = require('../utils/common');
 const { UNAUTHORIZED, MSG_MISSING_AUTH_HEADER } = require('../utils/constants');
 
-const extractBearerToken = (header) => header.replace('Bearer ', '');
-
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!req.cookies.jwt && (!authorization || !authorization.startsWith('Bearer '))) {
+  const { jwt: token } = req.cookies;
+
+  if (!token) {
     return res.status(UNAUTHORIZED).send(throwMessage(MSG_MISSING_AUTH_HEADER));
   }
-
-  const token = req.cookies.jwt ? req.cookies.jwt : extractBearerToken(authorization);
 
   let payload;
   try {
